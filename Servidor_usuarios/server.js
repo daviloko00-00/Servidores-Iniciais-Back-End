a818bd84e057d16be1670c30c9b929ec95a0b623
unknown <davicustodiosilva36@gmail.com>
2025-08-04 11:05:21 -0300

    Projeto de Servidores básicos com Js

---
+++ Servidor_usuarios/server.js
@@ -0,0 +1,12 @@
+
+// importa o Express para uso de suas funcionalidades
+const express = require("express");
+
+// criada instancia do Express que representa um app web
+const server = express();
+const port = 80;
+
+// criação de servidor, FICA NA ULTIMA LINHA
+server.listen(port, ()=>{
+    console.log("Servidor de usuários iniciado e rodando na porta 80! ")
+});
